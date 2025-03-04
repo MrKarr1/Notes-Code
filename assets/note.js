ce612856c01090593eae69fd5f3f6c7ccc5ccc51
Maxime <metalmax31@hotmail.fr>
2025-03-04 16:53:44 +0100

    04/03/2025

--- assets/note.js
+++ assets/note.js
@@ -30,3 +30,22 @@ document.querySelector('#langage').addEventListener('click', () => {
     div_note.style.display = "none";
     div_langage.style.display = "flex";
 });
+
+function search(input, table) {
+    document.getElementById(input).addEventListener('input', (e) => {
+        const researchValue = e.target.value.toLowerCase();
+        for (const row of document.getElementById(table).getElementsByTagName('tr')) {
+            const name = row.getAttribute('data-name') || '';
+            const description = row.getAttribute('data-description') || '';
+            const tag = row.getAttribute('data-tag') || '';
+            const folder = row.getAttribute('data-folder') || '';
+
+            if (name.includes(researchValue) || tag.includes(researchValue) || folder.includes(researchValue) || description.includes(researchValue)) {
+                row.style.display = 'table-row';
+            } else {
+                row.style.display = 'none';
+            }
+        }
+    });
+}
+
